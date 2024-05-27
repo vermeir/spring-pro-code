a29b2f5184552f2d6f77c6ba04d18904c3eb3c97
youri <youri.vermeir@gmail.com>
2024-05-27 12:07:22 +0200

    lab 7

--- lab/24-test/src/test/java/rewards/TestInfrastructureLocalConfig.java
+++ lab/24-test/src/test/java/rewards/TestInfrastructureLocalConfig.java
@@ -2,6 +2,7 @@ package rewards;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
@@ -9,6 +10,7 @@ import javax.sql.DataSource;
 /* TODO-05: Update this configuration class so that its
  *          beans are members of the "local" profile.
  */
+@Profile("local")
 @Configuration
 public class TestInfrastructureLocalConfig {
 
